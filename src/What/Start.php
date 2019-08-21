7713d2560795f809955aa35b36de667536b897ad
tg <707180869@qq.com>
2019-08-21 16:45:26 +0800

    composer 初始化

---
+++ src/What/Start.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 风过无痕
+ * Date: 2019/8/21
+ * Time: 16:37
+ */
+
+namespace Something;
+
+
+class Start
+{
+    public static function test(){
+        return 'hello composer';
+    }
+}
